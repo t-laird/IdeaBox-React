d38170622cff4b187d443763be8dbe21f6687883
Thomas Laird <t6r6l5@gmail.com>
2017-11-22 14:54:38 -0700

    add submit conditions and enter to submit + autofocus

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -8,6 +8,7 @@ class Form extends Component {
     this.updateBodyVal = this.updateBodyVal.bind(this);
     this.updateTitleVal = this.updateTitleVal.bind(this);
     this.submitCard = this.submitCard.bind(this);
+    this.enterToSubmit = this.enterToSubmit.bind(this);
   }
 
   updateBodyVal(e) {
@@ -22,20 +23,36 @@ class Form extends Component {
   }
 
   submitCard() {
-    this.props.makeCard(this.state.titleVal, this.state.bodyVal);
-    this.setState({
-      titleVal: '',
-      bodyVal: ''
-    });
+    if (this.state.titleVal !== '' && this.state.bodyVal !== ''){
+      this.props.makeCard(this.state.titleVal, this.state.bodyVal);
+      this.setState({
+        titleVal: '',
+        bodyVal: ''
+      });
+      this.nameInput.focus();
+    }
   }
 
+  enterToSubmit(e) {
+    if (e.key === 'Enter') {
+      this.submitCard();
+    }
+  }
 
   render() {
     return (
       <div className="Form">
-        <input type="text" value={this.state.titleVal} onChange={this.updateTitleVal} placeholder="Title" maxLength="40" autoFocus/>
+        <input 
+          type="text" 
+          value={this.state.titleVal}  
+          onChange={this.updateTitleVal} 
+          onKeyDown={this.enterToSubmit} 
+          placeholder="Title" 
+          maxLength="40" 
+          ref={(input) => { this.nameInput = input; }} 
+          autoFocus/>
         <span className="charsLeft">{40 - this.state.titleVal.length}</span>
-        <input type="text" value={this.state.bodyVal} onChange={this.updateBodyVal} placeholder="Body"/>
+        <input type="text" value={this.state.bodyVal} onChange={this.updateBodyVal} onKeyDown={this.enterToSubmit} placeholder="Body"/>
         <span className="charsLeft">{40 - this.state.bodyVal.length}</span>
         <button onClick={this.submitCard}>save</button>      
       </div>
